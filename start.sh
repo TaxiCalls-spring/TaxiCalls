8b55c7d58078d088778ae723d641a5d1b136738c
Rômulo Manciola Meloca <rmeloca@gmail.com>
2017-11-04 22:29:59 -0200

    improved start to restart specific container

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
-mvn clean package
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+#!/bin/bash
+
+if [ "$(whoami)" != 'root' ]; then
+    echo "You have no permission to run $0 as non-root user."
+    exit 1;
+fi
+
+if [ -z $1 ]; then
+	mvn clean package $2
+	docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+else
+	docker-compose -f docker-compose.yml -f docker-compose.dev.yml kill $1
+	cd $1
+	mvn clean package $2
+	cd ..
+	docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build $1
+fi
